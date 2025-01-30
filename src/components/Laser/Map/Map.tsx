729a90f155b1fdca160ed14a70e4a311687ce186
isaacfontbrito <isaac.rodrigues227@gmail.com>
2025-01-30 20:51:45 -0300

    mapa atualizado

--- src/components/Laser/Map/Map.tsx
+++ src/components/Laser/Map/Map.tsx
@@ -1,20 +1,13 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 
 import style from "./Map.module.css"
-import { useEffect } from "react";
+
 
 const position = [-4.174142, -40.940081]
 
-function SetViewOnPosition({ position }) {
-    const map = useMap()
-    useEffect(() => {
-        map.setView(position, 13)
-    }, [map, position])
-    return null
-}
-
 export function Mapa() {
+    
     // detecta se o usuario está no iphone
     const isIOS = /iPad|iphone|iPod/.test(navigator.userAgent)
 
@@ -22,10 +15,10 @@ export function Mapa() {
     const appleMapsUrl = `https://maps.apple.com/?daddr=${position[0]},${position[1]}`
 
     return (
-        <MapContainer  style={{height: "198px", width: "100%", borderRadius: "1px"}}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-
-                <SetViewOnPosition position={position}/>
+        <MapContainer   style={{height: "198px", width: "100%", borderRadius: "1px"}}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                       
+            />
                  
             <Marker position={position}>
                 <Popup >
