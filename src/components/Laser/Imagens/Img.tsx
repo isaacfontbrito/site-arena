b3b4888e472b24b3b97553195ff33880678369d7
isaacfontbrito <isaac.rodrigues227@gmail.com>
2025-01-31 18:20:38 -0300

    adição de auto play e img

--- src/components/Laser/Imagens/Img.tsx
+++ src/components/Laser/Imagens/Img.tsx
@@ -1,26 +1,34 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination } from 'swiper/modules'
-
+import { Swiper , SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination } from 'swiper/modules'
+import { Swiper as SwiperType } from 'swiper'
 import img1 from '../Imagens/images/jms-kFHz9Xh3PPU-unsplash.jpg'
 import img2 from '../Imagens/images/pexels-asphotograpy-1002703.jpg'
-
+import img3 from '../Imagens/images/pexels-pixabay-459728.jpg'
 
 import style from './Img.module.css'
+import { useState } from 'react'
 
 const images = [
     img1,
     img2,
+    img3,
 
 ]
 
 export default function ImageCarrousel() {
+    const [ isHovered, setIsHovered] = useState<SwiperType | null>(null)
+
     return (
         <Swiper 
             className={style.imageContainer}
             pagination={{clickable: true}}
-            modules={[Pagination]}
+            modules={[Pagination, Autoplay]}
             spaceBetween={50}
             slidesPerView={1}
+            autoplay={{delay: 2000, disableOnInteraction: false}} // Inicia o auto play quando hover
+            onSwiper={setIsHovered}
+            onMouseEnter={() => isHovered?.autoplay.start()}
+            onMouseLeave={() => isHovered?.autoplay.stop()}
         >
             {images.map((src, index) => (
                 <SwiperSlide className={style.images} key={index}>
