ce4f0fe7e28fe80f177d99af8139011a05961c31
isaacfontbrito <isaac.rodrigues227@gmail.com>
2025-02-01 11:10:37 -0300

    mudanças no cabeçalho

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,26 +5,23 @@ import arenaClub from '../../assets/arenaclube.png'
 
 export function Header() {
     return (
-        <div className={styles.header}>
-            <div>
+        <div className={styles.header}>        
                 <div className={styles.divImg}>
+                        <img className={styles.img1} src={arenaBeer} alt="" />
                     
                     <img className={styles.img2} src={arenaClub} alt="" />
                 </div>
                 
-            </div>
+            
             
 
             <div className={styles.sidebar}>
-                    <div className={styles.beer}> 
-                        <span><a href=".header"><img className={styles.img1} src={arenaBeer} alt="" /></a></span>
-                    </div>
                     <div className={styles.btnsEsplorer}>
-                        <span><a href=".seclazer">lazer</a></span>
+                        <span><a href="#">lazer</a></span>
                         <span><a href="#">Cardapio</a></span>
                         <span><a href="#">Gastronomia</a></span>
                         <span><a href="#">Bebidas</a></span>
-                        <span><a href="#">Escolinha</a></span>
+                        <span><a href="#">Escolinha de futebol</a></span>
                         <span><a href="#">Eventos</a></span>
                     </div>
                 
--- src/components/Laser/Lazer.tsx
+++ src/components/Laser/Lazer.tsx
@@ -6,7 +6,7 @@ export function Lazer() {
     return (
         <section className={style.seclazer}>
             <div className={style.divlazer}>
-                <h1>Desfrute de momentos inesquecíveis em um ambiente feito para relaxar, se divertir e celebrar a vida!" 🌿🍹🎶</h1>
+                <h1>Desfrute de momentos inesquecíveis em um ambiente feito para relaxar, se divertir e celebrar a vida!" 🏊🍹🎶</h1>
                 <div className={style.informes}>
                     <div className={style.imagens}>
                         <ImageCarrousel />
