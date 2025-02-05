f8db4907f27a678d41398b3eed1791c7790eae2d
isaacfontbrito <isaac.rodrigues227@gmail.com>
2025-02-05 10:47:57 -0300

    cadastro

--- src/components/Matricula/Registration.tsx
+++ src/components/Matricula/Registration.tsx
@@ -10,46 +10,59 @@ export function Registration() {
         idade: '',
     })
 
+    const [bloqueado, setBloqueado] = useState(false);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        
         setFormData({
             ...formData,
             [e.target.name]: e.target.value,
+            
         })
+        setBloqueado(true)
+        
     }
 
     
 
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
+            e.preventDefault()
+
+            if (bloqueado && formData.nome && formData.telefone && formData.idade) {
+        
+                const contato = '+5588996596317'
+                
+                const mensagem = `Olá! Gostaria de fazer uma matricula na escolinha de futebol.%0A
+                *nome:* ${formData.nome}%0A
+                *Telefone:* ${formData.telefone}%0A
+                *Idade:* ${formData.idade}%0A`
+                
+                const linkWhatsapp = `https://wa.me/${contato}?text=${mensagem}`
+                
+                window.open(linkWhatsapp, '_blank')
+            } else {
+                alert(' Preencha todos os campos antes de enviar!')
+            }
     }
 
-    const contato = '88996596317'
 
-    const mensagem = `Olá! Gostaria de fazer uma matricula na escolinha de futebol.%0A
-    *nome:* ${formData.nome}%0A
-    *Telefone:* ${formData.telefone}%0A
-    *Idade:* ${formData.idade}`
-
-    const linkWhatsapp = `https://wa.me/${contato}?text=${mensagem}`
-
-    window.open(linkWhatsapp, '_blank')
 
     return (
         <div className={style.registration} id='registration'>
             
             <div className={style.divForms}>
                 <h1>Formulario de matricula</h1>
-                <form >
+                <form onSubmit={handleSubmit}>
                     <label>Nome Completo:</label>
-                    <input type='text' name='nome' value={formData.nome} onChange={handleChange} required/>
+                    <input type='text' name='nome' value={formData.nome} onChange={handleChange} required placeholder='Digite seu nome aqui!'/>
                         
                     <label>Telefone:</label>
-                    <input type='text' name='telefone' value={formData.telefone} onChange={handleChange} required/>
+                    <input type='text' name='telefone' value={formData.telefone} onChange={handleChange} required placeholder='Contato'/>
                         
                     <label>Idade:</label>
-                    <input type='number' name='idade' value={formData.idade} onChange={handleChange} required/>
+                    <input type='number' name='idade' value={formData.idade} onChange={handleChange} required placeholder='Sua idade'/>
 
-                    <button onSubmit={handleSubmit} type='submit'>Enviar Matrícula <FaFutbol/></button>
+                    <button type='submit'>Enviar Matrícula <FaFutbol/></button>
                 </form>
             </div>        
         </div>
